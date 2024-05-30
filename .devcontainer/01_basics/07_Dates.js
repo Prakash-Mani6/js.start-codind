8c7f06a5e7867470eebe26ed5871acc326be03c0
Prakash-Mani6 <prakashmanidubey6@gmail.com>
2024-05-30 13:49:11 +0000

    dates

---
+++ .devcontainer/01_basics/07_Dates.js
@@ -0,0 +1,19 @@
+//  ******************** Date***********************
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(typeof myDate)
+
+// let myCreatedate = new Date(2024,0,23)
+// let myCreatedate = new Date(2024,0,23,7,6)
+let myCreatedate = new Date("01,02,2020")
+// console.log(myCreatedate.toLocaleString());
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedate.getTime());
+
