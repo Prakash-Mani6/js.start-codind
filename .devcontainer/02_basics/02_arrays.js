eba842495e07b0f2ed0786a13685f94221b6e172
Prakash-Mani6 <prakashmanidubey6@gmail.com>
2024-05-30 17:55:04 +0000

    array

--- .devcontainer/02_basics/02_arrays.js
+++ .devcontainer/02_basics/02_arrays.js
@@ -17,3 +17,14 @@ console.log(all_bikes_friends);                     //=> there is no limitestion
 const aaray = [1,3,2,4,[3,2,4,3],8,6,[5,6,5,3,[5,3,3,7,5]]]
 const one_array = aaray.flat(Infinity)       // => bassicaly show in one depth <=
 console.log(one_array);                 // output =>[1,3,2,4..........] => one defth
+
+console.log(Array.isArray("prakash"));
+console.log(Array.from("prakash"));           //=> convert to array <=
+console.log(Array.from({name: "prakash"}));    // tagda Quistions     
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
